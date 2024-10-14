9eb8e8312eb325f1207cb5a2514c1d22b7c2d3cd
Najib <najibburhaan@gmail.com>
2024-10-14 14:45:39 +0100

    done

---
+++ 8kyu/even-odd-numbers.js
@@ -0,0 +1,22 @@
+// Write a function called showNumbers that takes one parameter, limit. This function should display all the numbers from 0 up to the specified limit. Additionally, next to each number, indicate whether it is even or odd.
+
+const showNumbers = (limit) => {
+    for (let i = 0; i <= limit; i++) {
+        console.log(i, i % 2 === 0 ? "EVEN" : "ODD");
+    }
+};
+
+showNumbers(10);
+
+// Output:
+// 0 Even
+// 1 Odd
+// 2 Even
+// 3 Odd
+// 4 Even
+// 5 Odd
+// 6 Even
+// 7 Odd
+// 8 Even
+// 9 Odd
+// 10 Even
