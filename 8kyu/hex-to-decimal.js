99b05fdbe1b35199917b749ecfafee7e843bfbfd
Najib <najibburhaan@gmail.com>
2024-10-18 19:45:57 +0100

    done

---
+++ 8kyu/hex-to-decimal.js
@@ -0,0 +1,7 @@
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+// Solution
+const hexToDec = (hexString) => parseInt(hexString, 16)
+console.log(hexToDec("A")); // Output: 10
+
+
