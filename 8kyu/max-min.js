e196e60d92d12b0709d682869e50967f59b992cf
Najib <najibburhaan@gmail.com>
2024-10-22 23:49:06 +0100

    done

--- 8kyu/max-min.js
+++ 8kyu/max-min.js
@@ -13,4 +13,5 @@ var max = function(list){
 const min = list => Math.min(...list);
 const max = list => Math.max(...list);
 
-console.log(max([-52, 56, 30, 29, -54, 0, -110]))
+console.log(max([-52, 56, 30, 29, -54, 0, -110])) // Outpt: 56
+console.log(min([-52, 56, 30, 29, -54, 0, -110])) // Outpt: -110
