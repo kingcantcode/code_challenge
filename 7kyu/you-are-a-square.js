747b309ab4ace70cf425ce7d12647163c1847402
Najib <najibburhaan@gmail.com>
2024-11-04 00:37:43 +0000

    done

---
+++ 7kyu/you-are-a-square.js
@@ -0,0 +1,5 @@
+// Write a JavaScript function that checks if a given integer is a perfect square
+
+// Solution
+const isSquare = n =>  Math.sqrt(n) % 1 === 0; 
+console.log(isSquare(25)) // Output: true
